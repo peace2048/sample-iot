99ba7fa706997755c5b115fb54b0806d5510bbe5
ochiai <ochiai@ailes.co.jp>
2016-04-28 19:11:51 +0900

    WebAppを追加

---
+++ SampleIoT/WebApp/Startup.cs
@@ -0,0 +1,17 @@
+﻿using Owin;
+using Microsoft.Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApp
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
